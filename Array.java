e6ed6212a338c155290e6279a8f307d856228837
Elle <121001031+chelyie@users.noreply.github.com>
2024-07-19 14:54:09 +0800

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,15 @@
+public class array {
+
+	public static void main(String[] args) {
+		// Printing a string on screen 
+		// Using the println method 
+        System.out.println("Hello Madlang pips");
+		/*
+		 * this is a
+		 * comment in 
+		 * multiple lines in java
+		 */
+		
+	}
+
+}
